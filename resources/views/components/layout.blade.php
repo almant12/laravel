58203ccc5050b496188e1679477e948c59c3ba7b
Almant <dokoalmant123@gmail.com>
2024-01-20 15:44:14 +0100

    commit

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -81,7 +81,7 @@
 </main>
 
 <footer
-    class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
+    class="relative w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
 >
     <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
 
@@ -90,7 +90,8 @@
         class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
     >Post Job</a>
     @endauth
-</footer>
+</footer
+    class="relative w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
 <x-flash-message/>
 </body>
 </html>
